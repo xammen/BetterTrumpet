ec670c1b751b4dae82dee6b8827ab430c0fbeca0
GoldenTao <taoca@hotmail.com>
2015-08-01 19:59:53 +1000

    Fixed DPI scaling issue. Bumped rev.

---
+++ EarTrumpet/ViewModels/AppItemViewModelComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EarTrumpet.ViewModels
+{
+    public class AppItemViewModelComparer : IComparer<AppItemViewModel>
+    {
+        public static AppItemViewModelComparer Instance = new AppItemViewModelComparer();
+
+        public int Compare(AppItemViewModel one, AppItemViewModel two)
+        {
+            return string.Compare(one.DisplayName, two.DisplayName, StringComparison.Ordinal);
+        }
+    }
+}
