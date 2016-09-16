68cc680f4e7d8c7d097321f21d32e5b816eb550b
GoldenTao <taoca@hotmail.com>
2016-09-16 23:36:12 +1000

    Merge branch 'dev-project-c'

---
+++ EarTrumpet/Services/UserPreferencesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace EarTrumpet.Services
+{
+    public static class UserPreferencesService
+    {
+        // It's quiet in here.
+    }
+}
