fbb9a7da13f8d48047c5a0ec6016091842937282
Rafael Rivera <rafael@withinwindows.com>
2018-08-21 23:30:42 -0700

    Rev to 2.0.6.0 for backend localization fix (fixes #277)

---
+++ EarTrumpet/AppTrace.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace EarTrumpet
+{
+    class AppTrace : TraceListener
+    {
+        internal static AppTrace Instance;
+        internal StringBuilder Log = new StringBuilder();
+
+        private readonly DefaultTraceListener _defaultListener = new DefaultTraceListener();
+        private static Action<Exception> _warningCallback;
+
+        public static void Initialize(Action<Exception> onWarningException)
+        {
+            _warningCallback = onWarningException;
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new AppTrace());
+        }
+        
+        public static void LogWarning(Exception ex)
+        {
+            _warningCallback.Invoke(ex);
+        }
+
+        public AppTrace()
+        {
+            Debug.Assert(Instance == null);
+
+            Instance = this;
+        }
+
+        public override void Write(string message)
+        {
+            _defaultListener.Write(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            message = $"{DateTime.Now.ToString("HH:mm:ss.fff")} {message}";
+
+            Log.AppendLine(message);
+            _defaultListener.WriteLine(message);
+        }
+    }
+}
---
+++ EarTrumpet/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EarTrumpet.Interop;
+using System;
+using System.Runtime.InteropServices;
+
+namespace EarTrumpet.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static bool Is(this Exception ex, Error type)
+        {
+            switch(type)
+            {
+                case Error.AUDCLNT_E_DEVICE_INVALIDATED:
+                case Error.AUDCLNT_S_NO_SINGLE_PROCESS:
+                case Error.ERROR_NOT_FOUND:
+                    return (uint)(ex as COMException)?.HResult == (uint)type;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
