c3e0913f6167e559cffba0df855c698d480f6488
Rafael Rivera <rafael@withinwindows.com>
2019-02-21 00:12:37 -0800

    Aligned changelog with 2.0.8.0 release

---
+++ EarTrumpet/DataModel/DataModelFactory.cs
@@ -0,0 +1,31 @@
+﻿using EarTrumpet.DataModel.Internal;
+using EarTrumpet.UI.Services;
+
+namespace EarTrumpet.DataModel
+{
+    public class DataModelFactory
+    {
+        static IAudioDeviceManager s_playbackDevices;
+        static IAudioDeviceManager s_recordingDevices;
+
+        public static IAudioDeviceManager CreateAudioDeviceManager(AudioDeviceKind kind)
+        {
+            if (kind == AudioDeviceKind.Playback)
+            {
+                if (s_playbackDevices == null)
+                {
+                    s_playbackDevices = new AudioDeviceManager(AudioDeviceKind.Playback);
+                }
+                return s_playbackDevices;
+            }
+            else
+            {
+                if (s_recordingDevices == null)
+                {
+                    s_recordingDevices = new AudioDeviceManager(AudioDeviceKind.Recording);
+                }
+                return s_recordingDevices;
+            }
+        }
+    }
+}
---
+++ EarTrumpet/DataModel/IAudioDeviceManager.cs
@@ -0,0 +1,20 @@
+﻿using EarTrumpet.Interop.MMDeviceAPI;
+using System;
+
+namespace EarTrumpet.DataModel
+{
+    public interface IAudioDeviceManager
+    {
+        event EventHandler<IAudioDevice> DefaultChanged;
+        event EventHandler Loaded;
+
+        IAudioDevice Default { get; }
+        IAudioDeviceCollection Devices { get; }
+        AudioDeviceKind DeviceKind { get; }
+        void SetDefaultDevice(IAudioDevice device, ERole role = ERole.eMultimedia);
+        IAudioDevice GetDefaultDevice(ERole role = ERole.eMultimedia);
+        void MoveHiddenAppsToDevice(string appId, string id);
+        void SetDefaultEndPoint(string id, int pid);
+        string GetDefaultEndPoint(int processId);
+    }
+}
