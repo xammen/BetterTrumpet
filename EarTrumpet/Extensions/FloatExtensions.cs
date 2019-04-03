b4d8f8358044c81f16816f77631aa1613d51fc76
Rafael Rivera <rafael@withinwindows.com>
2019-04-03 04:23:52 -0700

    Release docs for 2.1.1.0

---
+++ EarTrumpet/Extensions/FloatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EarTrumpet.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static Int32 ToVolumeInt(this float val)
+        {
+            return Convert.ToInt32(Math.Round((val * 100), MidpointRounding.AwayFromZero));
+        }
+
+        public static float Bound(this float val, float min, float max)
+        {
+            return Math.Max(min, Math.Min(max, val));
+        }
+    }
+}
