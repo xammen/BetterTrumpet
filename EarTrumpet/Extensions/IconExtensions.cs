401386b9146c6bfd23b9f0bc38781036f48b9fb6
Rafael Rivera <rafael@withinwindows.com>
2019-04-03 03:22:25 -0700

    Merge branch 'release/2.1.0.0'

---
+++ EarTrumpet/Extensions/IconExtensions.cs
@@ -0,0 +1,33 @@
+﻿using EarTrumpet.Interop;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace EarTrumpet.Extensions
+{
+    public static class IconExtensions
+    {
+        public static ImageSource ToImageSource(this Icon icon)
+        {
+            var bitmap = icon.ToBitmap();
+            var hBitmap = bitmap.GetHbitmap();
+            ImageSource bitmapSource;
+            try
+            {
+                bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                Gdi32.DeleteObject(hBitmap);
+            }
+            return bitmapSource;
+        }
+    }
+}
